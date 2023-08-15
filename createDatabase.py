0df99309c4369f968b3600d1cc58daa75b4a653e
VakZok <tobias.krebs1999@gmx.de>
2023-08-15 17:19:00 +0200

    Modification #1 of the code to fit the input data

--- createDatabase.py
+++ createDatabase.py
@@ -1,123 +1,122 @@
 import sqlite3
 
-# connect to database (or create one, if there is non yet)
+# Mit Datenbank verbinden (oder eine erstellen, falls noch keine existiert)
 conn = sqlite3.connect('CustomerSales.db')
 
-# create cursor object -> allows interaction with DB using sql
+# Cursor-Objekt erstellen -> ermöglicht die Interaktion mit der DB über SQL
 cursor = conn.cursor()
 
-# Tabelle löschen (falls sie bereits existiert)
-cursor.execute('DROP TABLE IF EXISTS Kunden')
-cursor.execute('DROP TABLE IF EXISTS Produkte')
-cursor.execute('DROP TABLE IF EXISTS Regionen')
-cursor.execute('DROP TABLE IF EXISTS Linien')
-cursor.execute('DROP TABLE IF EXISTS Modelle')
-cursor.execute('DROP TABLE IF EXISTS Kategorien')
-cursor.execute('DROP TABLE IF EXISTS Verkauf')
-cursor.execute('DROP TABLE IF EXISTS Produktmodell')
-cursor.execute('DROP TABLE IF EXISTS Produktkategorie')
-cursor.execute('DROP TABLE IF EXISTS Linienmodell')
+# Tabellen löschen (falls sie bereits existieren)
+cursor.execute('DROP TABLE IF EXISTS kunde')
+cursor.execute('DROP TABLE IF EXISTS produkt')
+cursor.execute('DROP TABLE IF EXISTS region')
+cursor.execute('DROP TABLE IF EXISTS linie')
+cursor.execute('DROP TABLE IF EXISTS modell')
+cursor.execute('DROP TABLE IF EXISTS kategorie')
+cursor.execute('DROP TABLE IF EXISTS verkauf')
+cursor.execute('DROP TABLE IF EXISTS produktmodell')
+cursor.execute('DROP TABLE IF EXISTS produktkategorie')
+cursor.execute('DROP TABLE IF EXISTS linienmodell')
 
-# CREATE OR REPLACE TABLE-Anweisungen mit Checks für TEXT-Datentyp
+# CREATE TABLE-Anweisungen mit Checks für TEXT-Datentyp
 cursor.execute('''
-    CREATE TABLE Kunden (
-        KundenID INTEGER PRIMARY KEY,
-        Geburtstag DATE NOT NULL,
-        Vorname TEXT NOT NULL,
-        Nachname TEXT NOT NULL,
-        Adresse TEXT NOT NULL,
-        Geschlecht TEXT NOT NULL CHECK(Geschlecht IN ('M', 'W', 'D')),
-        Familienstand TEXT NOT NULL CHECK(Familienstand IN ('L', 'V')),
-        Bildungsstand TEXT NOT NULL CHECK(Bildungsstand IN ('Bachelor', 'Schulabschluss', 'Oberschule', 'unvollständiger Abschluss', 'unvollständige Oberschule')),
-        Beruf TEXT NOT NULL CHECK(Beruf IN ('Sachbearbeiter', 'Profi', 'Fachmann', 'Management', 'Handwerker')),
-        Jahreseinkommen INTEGER NOT NULL,
-        Hausbesitzer BOOLEAN NOT NULL,
-        Anzahl_Autos INTEGER NOT NULL
+    CREATE TABLE kunde (
+        kundenID INTEGER PRIMARY KEY,
+        vorname TEXT NOT NULL,
+        nachname TEXT NOT NULL,
+        geburtstag DATE NOT NULL,
+        familienstand TEXT NOT NULL CHECK(familienstand IN ('L', 'V')),
+        geschlecht TEXT NOT NULL CHECK(geschlecht IN ('M', 'W', 'D')),
+        jahreseinkommen INTEGER NOT NULL,
+        bildungsstand TEXT NOT NULL CHECK(bildungsstand IN ('Bachelor', 'Schulabschluss', 'Oberschule', 'unvollständiger Abschluss', 'unvollständige Oberschule')),
+        beruf TEXT NOT NULL CHECK(beruf IN ('Sachbearbeiter', 'Profi', 'Fachmann', 'Management', 'Handwerker')),
+        hausbesitzer BOOLEAN NOT NULL,
+        anzahl_autos INTEGER NOT NULL,
+        adresse TEXT NOT NULL
     )
 ''')
 
 cursor.execute('''
-    CREATE TABLE Produkte (
-        ProduktID INTEGER PRIMARY KEY,
-        Produktname TEXT NOT NULL,
-        Farbe TEXT NOT NULL CHECK(Farbe IN ('schwarz', 'weiss', 'grün', 'blau', 'rot', 'gelb', 'orange', 'violett', 'pink', 'braun', 'grau')),
-        Produktbeschreibung TEXT NOT NULL,
-        Herstellungszeit DATETIME NOT NULL,
-        Listenpreis NUMERIC NOT NULL
+    CREATE TABLE produkt (
+        produktID INTEGER PRIMARY KEY,
+        produktname TEXT NOT NULL,
+        farbe TEXT NOT NULL,
+        produktbeschreibung TEXT NOT NULL,
+        herstellungszeit INTEGER NOT NULL,
+        listenpreis NUMERIC(6,2)
     )
 ''')
 
 cursor.execute('''
-    CREATE TABLE Regionen (
-        RegionID INTEGER PRIMARY KEY,
-        Region TEXT NOT NULL CHECK(Region IN ('Baden-Württemberg', 'Hessen', 'Bayern', 'Nordrhein-Westfalen', 'Sachsen', 'Niedersachsen', 'Rheinland-Pfalz')),
-        Land TEXT NOT NULL CHECK(Land IN ('Deutschland', 'Frankreich', 'Schweiz', 'Österreich', 'Italien', 'Spanien', 'Niederlande')),
-        Gruppe TEXT NOT NULL CHECK(Gruppe IN ('Gruppe A', 'Gruppe B', 'Gruppe C', 'Gruppe D', 'Gruppe E', 'Gruppe F', 'Gruppe G')),
-        Preisaufschlag NUMERIC NOT NULL
+    CREATE TABLE region (
+        regionID INTEGER PRIMARY KEY,
+        region TEXT NOT NULL,
+        land TEXT NOT NULL,
+        gruppe TEXT NOT NULL,
+        preisaufschlag INTEGER NOT NULL
     )
 ''')
 
 cursor.execute('''
-    CREATE TABLE Linien (
-        LinienID INTEGER PRIMARY KEY,
-        LinienmodellID INTEGER,
-        Linienbezeichnung TEXT NOT NULL,
-        FOREIGN KEY (LinienmodellID) REFERENCES Linienmodelle(LinienmodellID)
+    CREATE TABLE linie (
+        linieID INTEGER PRIMARY KEY,
+        linienbezeichnung TEXT NOT NULL
     )
 ''')
 
 cursor.execute('''
-    CREATE TABLE Modelle (
-        ModellID INTEGER PRIMARY KEY,
-        Modellname TEXT NOT NULL
+    CREATE TABLE modell (
+        modellID INTEGER PRIMARY KEY,
+        modellname TEXT NOT NULL
     )
 ''')
 
 cursor.execute('''
-    CREATE TABLE Kategorien (
-        KategorieID INTEGER PRIMARY KEY,
-        Bezeichnung TEXT NOT NULL
+    CREATE TABLE kategorie (
+        kategorieID INTEGER PRIMARY KEY,
+        bezeichnung TEXT NOT NULL
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE verkauf (
-        VerkaufsID INTEGER PRIMARY KEY,
-        KundenID INTEGER NOT NULL,
-        ProduktID INTEGER NOT NULL,
-        RegionID INTEGER NOT NULL,
-        Datum DATE NOT NULL,
-        Anzahl INTEGER NOT NULL,
-        FOREIGN KEY (KundenID) REFERENCES Kunden(KundenID),
-        FOREIGN KEY (ProduktID) REFERENCES Produkte(ProduktID),
-        FOREIGN KEY (RegionID) REFERENCES Regionen(RegionID)
+        verkaufsID TEXT PRIMARY KEY,
+        datum DATE NOT NULL,
+        anzahl INTEGER NOT NULL,
+        kundenID INTEGER NOT NULL,
+        produktID INTEGER NOT NULL,
+        FOREIGN KEY (kundenID) REFERENCES kunde(kundenID),
+        FOREIGN KEY (produktID) REFERENCES produkt(produktID)
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE produktmodell (
-        ProduktID INTEGER NOT NULL,
-        ModellID INTEGER NOT NULL,
-        PRIMARY KEY (ProduktID, ModellID),
-        FOREIGN KEY (ProduktID) REFERENCES Produkte(ProduktID),
-        FOREIGN KEY (ModellID) REFERENCES Modelle(ModellID)
+        produktID INTEGER NOT NULL,
+        modellID INTEGER NOT NULL,
+        PRIMARY KEY (produktID, modellID),
+        FOREIGN KEY (produktID) REFERENCES produkt(produktID),
+        FOREIGN KEY (modellID) REFERENCES modell(modellID)
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE produktkategorie (
-        ProduktID INTEGER NOT NULL,
-        KategorieID INTEGER NOT NULL,
-        PRIMARY KEY (ProduktID, KategorieID),
-        FOREIGN KEY (ProduktID) REFERENCES Produkte(ProduktID),
-        FOREIGN KEY (KategorieID) REFERENCES Kategorien(KategorieID)
+        produktID INTEGER NOT NULL,
+        kategorieID INTEGER NOT NULL,
+        PRIMARY KEY (produktID, kategorieID),
+        FOREIGN KEY (produktID) REFERENCES produkt(produktID),
+        FOREIGN KEY (kategorieID) REFERENCES kategorie(kategorieID)
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE linienmodell (
-        LinienmodellID INTEGER PRIMARY KEY,
-        ModellID INTEGER NOT NULL
+        modellID INTEGER NOT NULL,
+        linielID INTEGER NOT NULL,
+        PRIMARY KEY (modellID, linielID),
+        FOREIGN KEY (modellID) REFERENCES modell(modellID),
+        FOREIGN KEY (linielID) REFERENCES linie(linielID)
     )
 ''')
 
