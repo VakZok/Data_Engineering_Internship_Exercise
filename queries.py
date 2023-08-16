7682d465843befad951e174ff46f205fcf81b816
VakZok <tobias.krebs1999@gmx.de>
2023-08-16 16:49:57 +0200

    Third query done sucessfully!

--- queries.py
+++ queries.py
@@ -7,7 +7,7 @@ connection = sqlite3.connect("CustomerSales.db")
 # Cursor erstellen
 cursor = connection.cursor()
 
-# Erste Abfrage ausführen
+# Query 1: Übersicht aller Verkäufe + Kunden-, Produkt-, und Regionsinformationen
 verkauf_kunde_produkt_region = (
     "SELECT verkauf.*, kunde.*, produkt.*, region.* "
     "FROM verkaufsregion "
@@ -17,10 +17,25 @@ verkauf_kunde_produkt_region = (
     "JOIN region ON verkaufsregion.regionID = region.regionID;"
 )
 
-result = pd.read_sql_query(verkauf_kunde_produkt_region, connection)
+verkauf_kunde_produkt_region_ergebnis = pd.read_sql_query(verkauf_kunde_produkt_region, connection)
+
+# Query 3: Produkte, die ein Modell besitzen, aber keine Produktlinie
+produkte_mit_Modell_ohne_Linie = (
+    "SELECT DISTINCT produkt.*, produktmodell.modellID, linienmodell.linienID "
+    "FROM produktmodell "
+    "JOIN produkt ON produktmodell.produktID = produkt.produktID "
+    "LEFT JOIN modell ON produktmodell.modellID = modell.modellID "
+    "LEFT JOIN linienmodell ON modell.modellID = linienmodell.modellID "
+    "WHERE linienmodell.modellID IS NULL; "
+)
+
+produkte_mit_Modell_ohne_Linie_ergebnis = pd.read_sql_query(produkte_mit_Modell_ohne_Linie, connection)
 
 # Die Ergebnisse anzeigen
-print(result)
+print("Query 1: Übersicht aller Verkäufe + Kunden-, Produkt-, und Regionsinformationen")
+print(verkauf_kunde_produkt_region_ergebnis)
+print("Query 3: Produkte, die ein Modell besitzen, aber keine Produktlinie")
+print(produkte_mit_Modell_ohne_Linie_ergebnis)
 
 # Den Cursor und die Verbindung schließen
 cursor.close()
